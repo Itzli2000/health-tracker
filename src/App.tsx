186d9768136368456f6b2347322da89272466475
Itzli Molina <itzli2000@msn.com>
2025-09-01 17:57:58 -0600

    feat(dependencies): add react-router for routing capabilities
    - Included react-router version 7.8.2 in package.json to enable routing within the application.
    - Updated pnpm-lock.yaml to reflect the addition of react-router and its dependencies.
    - Wrapped the App component in BrowserRouter to facilitate navigation and routing management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@components/ui/button";
+import { BrowserRouter } from "react-router";
 import "./App.css";
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="flex flex-col items-center justify-center h-screen bg-background">
-        <ModeToggle />
-        <h1>Hello World</h1>
-        <Button>Click me</Button>
-      </div>
+      <BrowserRouter>
+        <div className="flex flex-col items-center justify-center h-screen bg-background">
+          <ModeToggle />
+          <h1>Hello World</h1>
+          <Button>Click me</Button>
+        </div>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
